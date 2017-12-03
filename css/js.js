eb21d3b444637c43c2c4176fb549ccf5ed2cde75
JoseVitorp <32195814+JoseVitorp@users.noreply.github.com>
2017-12-03 18:07:31 -0200

    1.0
    versão inicial do meu blog

---
+++ css/js.js
@@ -0,0 +1,10 @@
+/*Função responsiva do menu*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
